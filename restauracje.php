5f0a2713fd72ed3df29b4584bb085baad40dcd39
beryll-ops <75722271+beryll-ops@users.noreply.github.com>
2021-05-25 14:36:56 +0200

    php

---
+++ restauracje.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="pl" dir="ltr">
+   <head>
+      <meta charset="utf-8">
+      <title>Powiat międzychodzki</title>
+      <link rel="stylesheet" href="stylesheet1.css">
+   </head>
+   <body>
+      <img src="herb_logo.png" style="float: left;" usemap="#akukaracza" class="akukaracza">
+      <map name="akukaracza">
+        <area shape="poly" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" coords="0,0,124,0,124,60,0,60">
+      </map>
+      <ul class="tlo">
+        <li>
+         <img src="puste-png.png" class="tlo">
+        </li>
+      </ul>
+      <ul class="ul">
+       <li>
+       <div class="menu1">
+        <button class="dropbutton1">Top Hotele na terenie powiatu</button>
+         <div class="menu-content1">
+           <a href="http://www.hotel-neptun.com.pl/">Hotel Neptun</a> <br>
+           <a href="http://www.jaroszewskiewzgorze.pl/">Hotel Sieraków</a> <br>
+         </div>
+       </div>
+       </li>
+       <li>
+       <div class="menu2">
+         <button class="dropbutton2">Najciekawsze Zabytki</button>
+           <div class="menu-content2">
+             <a href=""></a> <br>
+             <a href=""></a> <br>
+             <a href=""></a> <br>
+             <a href=""></a> <br>
+             <a href=""></a> <br>
+             <a href=""></a> <br>
+           </div>
+      </div>
+     </li>
+     <li>
+      <div class="menu3">
+         <button class="dropbutton3">Gminy w powiecie</button>
+           <div class="menu-content3">
+             <a href="https://miedzychod.pl/">Międzychód</a> <br>
+             <a href="https://kwilcz.pl/">Kwilcz</a> <br>
+             <a href="https://www.sierakow.pl/asp/pl_start.asp?typ=14&submenu=1&menu=1&strona=1">Sieraków</a> <br>
+             <a href="http://chrzypsko.pl/">Chrzypsko Wielkie</a> <br>
+           </div>
+      </div>
+     </li>
+     <li>
+      <div class="menu4">
+         <button class="dropbutton4">Mapa powiatu</button>
+           <div class="menu-content4">
+             <a href="https://www.google.pl/maps/place/mi%C4%99dzychodzki/@52.6014486,15.9853243,11.25z/data=!4m5!3m4!1s0x470698e2d0bb22e5:0x3020e32ad0ed5c0!8m2!3d52.6004652!4d16.0491591">Mapa</a> <br>
+           </div>
+      </div>
+     </li>
+     <li>
+      <div class="menu5">
+         <button class="dropbutton5">Restauracje</button>
+           <div class="menu-content5">
+             <a href="restauracje.php">Lista restauracji</a> <br>
+           </div>
+      </div>
+     </li>
+     </ul>
+      <div class="tresc">
+         <h3 class=naglowek-1></h3>
+         <p class="tresc-1"></p><br>
+         <h3 class=naglowek-1></h3>
+         <p class="tresc-1"></p> <br>
+         <h3 class=naglowek-1></h3>
+         <p class="tresc-1"></p> <br>
+         <h3 class=naglowek-1></h3>
+         <p class="tresc-1"></p> <br>
+         <h3 class=naglowek-1></h3>
+         <p class="tresc-1"></p> <br>
+      </div>
+      <div class="clock-container">
+         <div class="clock-number num1">
+            <div>1</div>
+         </div>
+         <div class="clock-number num2">
+            <div>2</div>
+         </div>
+         <div class="clock-number num3">
+            <div>3</div>
+         </div>
+         <div class="clock-number num4">
+            <div>4</div>
+         </div>
+         <div class="clock-number num5">
+            <div>5</div>
+         </div>
+         <div class="clock-number num6">
+            <div>6</div>
+         </div>
+         <div class="clock-number num7">
+            <div>7</div>
+         </div>
+         <div class="clock-number num8">
+            <div>8</div>
+         </div>
+         <div class="clock-number num9">
+            <div>9</div>
+         </div>
+         <div class="clock-number num10">
+            <div>10</div>
+         </div>
+         <div class="clock-number num11">
+            <div>11</div>
+         </div>
+         <div class="clock-number num12">
+            <div>12</div>
+         </div>
+         <div class="clock-hand" id="sec">
+            <div class="second-hand"></div>
+         </div>
+         <div class="clock-hand" id="min">
+            <div class="minute-hand"></div>
+         </div>
+         <div class="clock-hand" id="hr">
+            <div class="hour-hand"></div>
+         </div>
+      </div>
+      <script src="zegar.js"></script>
+   </body>
+</html>
